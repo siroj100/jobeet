a80eac639cec0e3522a1e04c4f16af2f8d5a516d
Sirajuddin Maizir <siroj100@gmail.com>
2010-04-17 22:38:15 +0700

    jobeet day 04

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -12,15 +12,15 @@ class jobActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
   {
-    $this->jobeet_jobs = Doctrine::getTable('JobeetJob')
+    $this->jobeet_job_list = Doctrine::getTable('JobeetJob')
       ->createQuery('a')
       ->execute();
   }
 
   public function executeShow(sfWebRequest $request)
   {
-    $this->jobeet_job = Doctrine::getTable('JobeetJob')->find(array($request->getParameter('id')));
-    $this->forward404Unless($this->jobeet_job);
+    $this->job = Doctrine::getTable('JobeetJob')->find(array($request->getParameter('id')));
+    $this->forward404Unless($this->job);
   }
 
   public function executeNew(sfWebRequest $request)
